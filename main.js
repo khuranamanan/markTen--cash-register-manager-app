df2013b92d700056eb9b5dbdf47effeac8b0704a
Manan Khurana <112554344+khuranamanan@users.noreply.github.com>
2022-09-25 12:23:44 +0530

    added logic with JS

---
+++ main.js
@@ -0,0 +1,41 @@
+var inputBillAmount = document.querySelector(".input-Bill-Amount");
+var inputCashGiven = document.querySelector(".input-Cash-Given");
+var btnCheck = document.querySelector("#btn-Check");
+var errorMessage = document.querySelector(".error-message");
+var outputNotes = document.querySelectorAll(".output-notes");
+
+var currencyAvailable = [2000, 500, 100, 20, 10, 5, 1];
+
+function error(msg){
+    errorMessage.style.display = "block";
+    errorMessage.innerText = msg;
+}
+
+function hideMessage(){
+    errorMessage.style.display = "none";
+}
+
+btnCheck.addEventListener("click", () =>{
+    hideMessage();
+    if (inputBillAmount.value > 0){
+        if (inputCashGiven.value >= inputBillAmount.value){
+            var balanceAmount = inputCashGiven.value - inputBillAmount.value;
+            notesToGive(balanceAmount);
+        }
+        else{
+            error("Cash given should be greater or equal to Bill Amount")
+        }
+    } else {
+        error("Enter a valid Bill Amount");
+    }
+});
+
+function notesToGive(Amount){
+    for(let i = 0; i < currencyAvailable.length; i++){
+        var numberOfNotes = Math.trunc(Amount/currencyAvailable[i]);
+        outputNotes[i].innerText = numberOfNotes;
+
+        Amount = Amount % currencyAvailable[i];
+
+    }
+}
